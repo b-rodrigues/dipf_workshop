d66b6381f860e99a0e0b928d60c7a18d2ce9cdeb
Bruno Rodrigues <bruno@brodrigues.co>
2024-05-11 11:16:13 +0200

    typos

--- _targets.R
+++ _targets.R
@@ -2,13 +2,9 @@ library(targets)
 library(tarchetypes)
 
 list(
-  tar_target(
-    life_cycle_savings,
-    read.csv("life_cycle_savings.csv")
-  ),
   tar_force(
     name = paper,
-    command = quarto::quarto_render("fosdem_pres.qmd"),
+    command = quarto::quarto_render("dipf_pres.qmd"),
     format = "file", 
     force = TRUE
   )
